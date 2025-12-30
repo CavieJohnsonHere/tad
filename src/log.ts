bbf4d0469a1ff5a3dcf1d0ffa8ca2b6182b0b545
Mohammad Dargahi <caviejohnsonhere@gmail.com>
2025-12-30 21:13:23 +0330

    changed: log files to be .log instead of .txt

--- src/log.ts
+++ src/log.ts
@@ -5,5 +5,5 @@ let logFileContent = "Started";
 export function log(entry: any) {
   const date = new Date(Date.now());
   logFileContent += `\n${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} - ${JSON.stringify(entry)}`;
-  writeFileSync("./logs.txt", logFileContent);
+  writeFileSync("./logs.log", logFileContent);
 }
