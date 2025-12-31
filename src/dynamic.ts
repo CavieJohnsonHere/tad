c227c3ca7b71ffce1247b1c2ad250a02370c1edc
Mohammad Dargahi <caviejohnsonhere@gmail.com>
2025-12-31 21:43:53 +0330

    changed: how dynamic works

--- src/dynamic.ts
+++ src/dynamic.ts
@@ -6,14 +6,18 @@ import { type Color, type Node, type RenderContext, colorize } from "./tui";
  * @param content - a function that returns some content that can be passed to the parent
  * @returns A Node-like API with layout configuration methods
  */
-export const dynamic = (content: () => string[]) => {
+export const dynamic = (content: () => Node) => {
   const api = {
     /**
      * Render, for internal use
      * (~~DO NOT USE YOURSELF~~ it just returns what is gonna be rendered without putting it on screen so you can use it if you really want to)
      */
-    _render(_: RenderContext) {
-      return content();
+    _render(
+      ctx: RenderContext,
+      allowedWidth: number,
+      selectedItem: [number, number]
+    ) {
+      return content()._render(ctx, allowedWidth, selectedItem);
     },
   };
 
